5a6a1ba40751ca31d8da3f4d5e34b1531b8a89b9
japheth <jzyenom@gmail.com>
2025-07-21 11:45:36 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/newsAndEvents", newsAndEvents);
 
 // Serve static files from the public folder
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // //Test
 app.get("/", (req, res) => {
