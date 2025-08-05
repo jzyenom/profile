da8abf2ca722de5d1a496ca8f29c78b4ddcd91aa
japheth <jzyenom@gmail.com>
2025-08-05 19:21:06 +0100

    fix posts getting

--- frontend/src/store/projectStore.js
+++ frontend/src/store/projectStore.js
@@ -2,7 +2,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://api-kebbi-government-profile.onrender.com/api/auth";
+const API_URL = "https://api-kebbi-government-profile.onrender.com/api/posts";
 
 // const API_URL = "http://localhost:5000/api/posts";
 
